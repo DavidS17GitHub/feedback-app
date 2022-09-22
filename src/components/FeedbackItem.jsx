fd61eb97c4f08cd3e35534734bffe40c0fc9ef5a
DavidS17GitHub <108815743+DavidS17GitHub@users.noreply.github.com>
2022-09-22 15:55:24 -0500

    initial commit for the feedback app

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,36 @@
+
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import Card from "./shared/Card"
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+import PropTypes from 'prop-types'
+
+
+function FeedbackItem({ item }) {
+  const {deleteFeedback, editFeedback } = useContext(FeedbackContext)
+
+  return (
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <button 
+            onClick={() => deleteFeedback(item.id)} 
+            className='close'>
+              <FaTimes color='#C41E3A'/>
+        </button>
+        <button 
+          onClick={() => editFeedback(item)}
+          className="edit">
+          <FaEdit color='purple'/>
+        </button>
+        <div className="text-display">
+            {item.text}
+        </div>
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+
+export default FeedbackItem
